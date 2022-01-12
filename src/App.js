586c4e6f312bbcb35e4a7521c990c7f4806058f9
Your Name <mari@Maribels-MacBook-Pro.local>
2022-01-11 18:01:48 -0600

    header is on there, just gettig started

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return(<div>
-      Github Card
+      <h1>Github Card</h1>
+      <form>
+        <input placeholder='Github Handle' />
+        <button>Search</button>
+      </form>
     </div>);
   }
 }
