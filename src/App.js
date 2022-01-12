711b1ec63312eae903762906944172676a407013
Your Name <mari@Maribels-MacBook-Pro.local>
2022-01-11 18:39:38 -0600

    followers are all in place

--- src/App.js
+++ src/App.js
@@ -8,6 +8,31 @@ class App extends React.Component {
         <input placeholder='Github Handle' />
         <button>Search</button>
       </form>
+
+      <div id='UserCard'>
+      <img width='300px' src="https://avatars.githubusercontent.com/u/93017587?v=4" />
+      <a target="_blank" href="https://github.com/Mbravo36"> <h3>Maribel Garcia</h3></a>
+      <p>(Mbravo36)</p>
+      <p>Total Repos: 33</p>
+      <p>Total Followers: 2</p>
+      </div>
+
+      <div id='followers'>
+        <div className='follower'>
+        <img width='200px' src="https://avatars.githubusercontent.com/u/31264591?v=4" />
+       <a target="_blank" href="https://github.com/FftyShadesofCode"> <p>Philip R McDavid</p></a>
+        </div>
+        <div className='follower'>
+        <img width='200px' src="https://avatars.githubusercontent.com/u/86445750?v=4" />
+       <a target="_blank" href="https://github.com/cainpalmer"> <p>Cain Palmer</p></a>
+        </div>
+        <div className='follower'>
+        <img width='200px' src="https://avatars.githubusercontent.com/u/91563653?v=4" />
+       <a target="_blank" href="https://github.com/kimnivore"> <p>Kim</p></a>
+        </div>
+
+      </div>
+
     </div>);
   }
 }
